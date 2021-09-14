ce6372299b04eb7021f136129b2f41ff5a3e105f
jasmeets7 <sing6443@sheridancollege.ca>
2021-09-14 15:59:21 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Spacestagram
+- [Deployed App] 
+- [Link to repo] (https://github.com/jasmeets7/Spacestagram.git)
+
+---
+
+## Installation and Usage
+
+**Run the following in your terminal:**
+
+```bash
+git clone https://github.com/jasmeets7/Spacestagram.git
+cd spacestagram
+npm install
+ng serve
+```
+
+Visit http://localhost:4200 in your browser to see the app running!
---
+++ src/app/shared/header/header.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { NasaService } from '../../nasa.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  url = window.location.origin;
+
+  constructor(public service:NasaService) { }
+
+  ngOnInit(): void { }
+
+
+  onDateChange(event:any){
+    const selectedDate = new Date(event.target.value);
+    const currentDate = new Date();
+    if (selectedDate < currentDate) 
+      this.service.getNasaDateData(event.target.value);
+    else {
+      alert("Select a valid Date");
+      event.target.value = "";
+    };
+  }
+
+}
