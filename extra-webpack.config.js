6416c63b5d203d5cfef4af12e69f5d93f7183027
Jasmeet Singh <52837712+jasmeets7@users.noreply.github.com>
2024-10-02 19:25:11 +0530

    fix: env

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -4,5 +4,6 @@ const isProd = process.env.NODE_ENV === 'production';
 module.exports = {
   plugins: [
     ...(!isProd ? [new Dotenv({ path: './.env' })] : []),
+    ...(isProd ? [new Dotenv({ systemvars: true })] : []),
   ],
 };
